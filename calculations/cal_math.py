a2cfc50b65f0ff612daa6a96d5164557400c86fb
Ste <stephan.obrien@mcgill.ca>
2025-02-21 10:23:06 -0500

    adding doc strings

--- calculations/cal_math.py
+++ calculations/cal_math.py
@@ -1,15 +1,19 @@
 def add(a, b):
+    """function to add a and b"""
     return a + b
 
 
 def sub(a, b):
+    """function to sub a and b"""
     return a - b
 
 
 def mult(a, b):
+    """function to mult a and b"""
     return a * b
 
 
 def div(a, b):
+    """function to div a and b"""
     return a / b
 
